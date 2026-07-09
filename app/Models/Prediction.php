31d66c64f3eb9aa7751b187e0a2c8c31432a42c5
Jeancito99 <jp_9912@hotmail.com>
2026-07-09 18:00:27 -0500

    v3

--- app/Models/Prediction.php
+++ app/Models/Prediction.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prediction extends Model
 {
-    //
+    // Habilitamos las columnas para que Laravel permita guardarlas con ::create()
+    protected $fillable = [
+        'temperatura',
+        'humedad',
+        'dias_restantes',
+        'estado'
+    ];
 }
